c9a8788c0cb73b38b68b6b050d773c69de518c7c
alex <alex@debian>
2018-05-11 00:21:02 +0700

    Change Integrate Gaussian Cube

--- work/Plot-Diazo-TD/PlotOrcaMO/3D/IntegrateGaussianCube.php
+++ work/Plot-Diazo-TD/PlotOrcaMO/3D/IntegrateGaussianCube.php
@@ -6,7 +6,21 @@ function CompactArray( $src ) {
     return $dst;
 }
 
-function ReadGaussianCube($csvFile) {
+function SumArray( $src, &$N ) {
+    $Sum = 0.0;
+    foreach ($src as $value) { $Sum += $value*$value; $N++; }
+    return $Sum;
+}
+
+function PrintArray( $src ) {
+    echo "[ ";
+    foreach ($src as $value) echo " $value;";
+    echo "] ";
+}
+
+function IntegrateGaussianCube($csvFile) {
+    $Num = 0;
+    $Sum = 0.0;
     $r = array();
     $file_handle = fopen($csvFile, 'r');
 
@@ -14,24 +28,26 @@ function ReadGaussianCube($csvFile) {
     while (!feof($file_handle) ) {
 	$line = CompactArray(fgetcsv($file_handle, 40000, " "));
 	$n++;
-	if($n>20) break;
+	if($n>19) break;
 //	if(count($line)>5) break;
     }
 
-    $n=0;
+    $n=0; $Num=0;
     while (!feof($file_handle) ) {
         $line = CompactArray(fgetcsv($file_handle, 40000, " "));
+	$Sum += SumArray($line, $Num);
+	PrintArray($line);
 	$n++;
-	var_dump($line);
+//	var_dump($line);
 	printf("%ld %ld\n", $n, count($line));
 //	$r[] = $line;
     }
     fclose($file_handle);
-    return $r;
+    echo "Num = $Num;    Sum = $Sum\n";
+    return $Sum;
 }
 
 $Name = $argv[1];
-$Cube = ReadGaussianCube($Name);
+$Cube = IntegrateGaussianCube($Name);
 
 ?>
-
